9e342620d861eb061c42f3a4ab8226ee145fa175
chyccs <chyccs@naver.com>
2023-06-22 19:04:10 +0900

    aaa

--- src/main/tests/test_manage.py
+++ src/main/tests/test_manage.py
@@ -1,11 +1,6 @@
 import unittest
 
-from manage import (
-    _decorate_bump,
-    _highlight,
-    _parse_title,
-    _tokenize,
-)
+from manage import _required
 
 
 class TestManage(unittest.TestCase):
@@ -24,45 +19,9 @@ class TestManage(unittest.TestCase):
     def tearDown(self):
         print('tearDown')
 
-    def test_tokenize(self):
-        self.assertEqual(_tokenize('return title'), 'title')
-        self.assertEqual(_tokenize('TITLE'), 'title')
-        self.assertEqual(_tokenize('make_canvas'), 'make canvas')
-        self.assertEqual(_tokenize('makeCanvas'), 'make canvas')
-
-    def test_decorate_bump(self):
-        self.assertEqual(_decorate_bump(
-            title='build(deps-dev): bump mypy from 0.991 to 1.0.0',
-            ref_name='dependabot/pip/mypy-1.0.0',
-        ), 'build(deps-dev): bump `mypy` from `0.991` to `1.0.0`')
-
-    def test_highlight(self):
-        keywords = [
-            'update pull requests',
-            'update_pull_requests',
-            'update pull request',
-            'update_pull_request',
-            'fetch_pull_requests',
-            'fetch pull requests',
-            'fetch github repos',
-            'fetch_github_repos',
-            'fetch pull request',
-            'fetch_pull_request',
-            'fetch_github_repo',
-            'fetch github repo',
-            'stopwords',
-            'stopword',
-        ]
-        self.assertEqual(_highlight(
-            text='feat: add testcases named test manage',
-            keywords=keywords,
-        ), 'feat: add testcases named test manage')
-
-    def test_parse_title(self):
-        self.assertEqual(_parse_title(title='feat: add testcases named test manage'),
-                         ('feat', 'add testcases named test manage'))
-        self.assertEqual(_parse_title(title='(feat)add testcases named test manage'),
-                         ('feat', 'add testcases named test manage'))
+    def test_required(self):
+        self.assertFalse(_required('return title'))
+        self.assertTrue(_required('fill me'))
 
 
 if __name__ == '__main__':
--- src/manage.py
+++ src/manage.py
@@ -1,37 +1,9 @@
 import inspect
-import keyword
 import os
-import re
-from os import environ as env
-from typing import (
-    List,
-    Set,
-)
 
 import openai
-from inflection import (
-    humanize,
-    pluralize,
-    singularize,
-    underscore,
-)
 
 from main import fetch_pull_request
-from main.constants import STOPWORDS
-
-TAG = [
-    'build',
-    'chore',
-    'ci',
-    'docs',
-    'feat',
-    'fix',
-    'perf',
-    'refactor',
-    'revert',
-    'style',
-    'test',
-]
 
 
 def _logging(level: str, title: str, message: str):
@@ -43,100 +15,24 @@ def _required(title: str):
     return 'fill me' in title.lower()
 
 
-def __can_relocate_words(title: str):
-    return ':' not in title
-
-
-def _decorate_number(title: str):
-    return re.sub(r'(([`]*)([0-9]+[0-9\.\-\%\$\,]*)([`]*))', r'`\3`', title)
-
-
-def _decorate_bump(title: str, ref_name: str):
-    decorated = _decorate_number(title)
-    match = re.search(r'dependabot\/\w+\/([\w\-]+)\-[\.\d]+', ref_name)
-    if match:
-        dep_name = match[1]
-        decorated = _highlight(decorated, {dep_name})
-    return decorated
-
-
-def _parse_title(title: str):
-    if __can_relocate_words(title):
-        p = re.search(r'(.*)[(\[](.*)[)\]](.*)', title)
-        if not p:
-            return ('misc', title)
-        plain_title = f'{p[1]}{p[3]}'
-        tag = p[2].lower().strip()
-        return tag, plain_title
-
-    p = re.search(r'(.*)[\:][ ]*(.*)', title)
-    return (p[1].lower().strip(), p[2].lower().strip()) if p else (None, None)
-
-
-def _highlight(text: str, keywords: Set[str]):
-    highlighted = text
-    for k in keywords:
-        try:
-            highlighted = re.sub(rf'\b(?<!`)({k})(?!`)\b', r'`\1`', highlighted)
-        except re.error as ex:
-            _logging('error', f'regex error during highlighting keyword {k}', str(ex))
-            continue
-        except Exception as ex:
-            _logging('error', f'misc error during highlighting keyword {k}', str(ex))
-            continue
-    return highlighted
-
-
-def _extend_singularize(symbols: List[str]):
-    symbols.extend([singularize(symbol) for symbol in symbols])
-
-
-def _extend_pluralize(symbols: List[str]):
-    symbols.extend([pluralize(symbol) for symbol in symbols])
-
-
-def _extend_files(symbols: List[str], src_path: str):
-    files = []
-    for root, _, f_names in os.walk(src_path):
-        for f in f_names:
-            file_path = os.path.join(root, f)
-            if file_path.startswith('./.'):
-                continue
-            files.append(f)
-    symbols.extend(files)
-
-
-def _tokenize(symbol: str):
-    stopwords = list(keyword.kwlist)
-    stopwords.extend(STOPWORDS)
-    return (re
-            .sub(rf'\b({"|".join(stopwords)})\b', r'', humanize(underscore(symbol)).lower().strip())
-            .lower().strip())
-
-
-def _symbolize(raw_symbols: str):
-    symbols = [_tokenize(symbol)for symbol in raw_symbols.split('\n') if len(_tokenize(symbol)) > 3]
-    symbols.extend([symbol.replace(' ', '_') for symbol in symbols])
-    return symbols
-
-
 def main():
-
     openai.organization = os.getenv("open_ai_org", "org-JAnMEEEFNvtPSGwRA1DVF3Yu")
     openai.api_key = os.getenv("open_ai_api_key", "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")
 
     pull_request = fetch_pull_request(
-        access_token=os.getenv("access_token", "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"),
-        owner=os.getenv("owner", "chyccs"),
-        repository=os.getenv("repository", "pull-request-generator"),
-        number=int(os.getenv("pull_request_number", "4"),),
+        access_token=os.getenv("access_token"),
+        owner=os.getenv("owner"),
+        repository=os.getenv("repository"),
+        number=int(os.getenv("pull_request_number")),
     )
 
-    # if not _required(pull_request.title):
-    #     return
-    
-    patches = ['Can you summarize the code changes I\'m about to enter in single lowercase Conventional Commits 1.0.0 format?']
-    
+    if not _required(pull_request.title):
+        return
+
+    patches = [
+        'Can you summarize the code changes I\'m about to enter in single lowercase Conventional Commits 1.0.0 format?',
+    ]
+
     for f in pull_request.get_files():
         patches.append(f.patch)
 
@@ -153,11 +49,12 @@ def main():
         stop=["\"\"\""],
     )
 
-    print(response)
+    _logging(level='info', title='pull-request topic created', message=response['choices'][0]['text'])
 
     pull_request.edit(
         title=(response['choices'][0]['text']),
     )
+
 
 if __name__ == "__main__":
     main()
